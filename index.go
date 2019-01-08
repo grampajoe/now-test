d1848fd982cd2e9dbb29c1370aef00e3c6ef71f1
Joe Friedl <stuff@joefriedl.net>
2019-01-08 17:38:34 -0500

    Update index.go

--- index.go
+++ index.go
@@ -2,9 +2,11 @@ package main
 
 import (
   "fmt"
+  "log"
   "net/http"
 )
 
 func Handler(w http.ResponseWriter, r *http.Request) {
+  log.Printf("got a request! headers: %v", r.Header)
   fmt.Fprintf(w, "hello")
 }
